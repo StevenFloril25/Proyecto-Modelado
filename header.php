33e927963edf364f545fd0d8d87700961c05fbc4
StevenFloril25 <jsfloril@espe.edu.ec>
2024-06-18 01:02:34 -0500

    V2

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="./public/fontawesome-free-6.5.2-web/css/all.css">
     <title>Crud Mongo </title>
     <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 </head>
 <body>
     
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="./index.php">Gestión de Eventos SANA SANA</a>
+    <a class="navbar-brand" href="./index.php">Gestión de Eventos Zorrito Rayado</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,5 +25,40 @@
   </div>
 </nav>
 
+<div class="hero-section">
+  <div class="container">
+    <h1 class="animate__animated animate__fadeInDown">Bienvenidos a Gestión de Eventos Zorrito Rayado</h1>
+    <p class="animate__animated animate__fadeInUp">Creamos eventos inolvidables para ti</p>
+  </div>
+</div>
+
+<div class="container services">
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card animate__animated animate__zoomIn">
+        <div class="card-body">
+          <h5 class="card-title">Organización de Eventos</h5>
+          <p class="card-text">Desde bodas hasta conferencias, lo organizamos todo.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card animate__animated animate__zoomIn">
+        <div class="card-body">
+          <h5 class="card-title">Decoración</h5>
+          <p class="card-text">Decoraciones temáticas que impresionarán a tus invitados.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card animate__animated animate__zoomIn">
+        <div class="card-body">
+          <h5 class="card-title">Catering</h5>
+          <p class="card-text">Deliciosas opciones de catering para cualquier tipo de evento.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include 'script.php'; ?>
